92fa6b140624849c706963805ec1d0a11e6f90f4
kostandesi24-meet <139680794+kostandesi24-meet@users.noreply.github.com>
2023-07-23 18:28:55 +0300

    still working

--- authentication-lab/app.py
+++ authentication-lab/app.py
@@ -13,12 +13,31 @@ def signin():
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
-    return render_template("signup.html")
+    if request.method == 'POST':
+        try: 
+            login_session['user'] = auth.create_user_with_email_and_password(form.request['email'] ,form.request['password'])
+            return redirect(url_for('add_tweet'))
+        except:
+            return render_template("signup.html" , error = "auth error")
+    return render_template('signup.html')
 
 
 @app.route('/add_tweet', methods=['GET', 'POST'])
 def add_tweet():
     return render_template("add_tweet.html")
+
+Config = {
+    "apiKey": "AIzaSyABkRKeFBOffKXpU5N1ww1RDbPIboHmchM",
+    "authDomain": "labtime-7afa9.firebaseapp.com",
+    "projectId": "labtime-7afa9",
+    "storageBucket": "labtime-7afa9.appspot.com",
+    "messagingSenderId": "254101586962",
+    "appId": "1:254101586962:web:aba5aeb5945b63b0e704cb",
+    "measurementId": "G-4PQNRTGY1Q",
+    "databaseURL":""
+}
+firebase = pyrebase.initialize_app(Config)
+auth = firebase.auth()
 
 
 if __name__ == '__main__':
